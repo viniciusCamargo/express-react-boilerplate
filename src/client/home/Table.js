1d433d17d3e418b863670ebf3cb9b773a1681e34
Vinicius Camargo <cmrg.vinicius@gmail.com>
2017-12-22 08:28:41 -0200

    performance improvements

--- src/client/home/Table.js
+++ src/client/home/Table.js
@@ -10,14 +10,16 @@ const User = ({ id, name, address, email, phone }) => (
 )
 
 export default ({ users }) => (
-  <tbody>
-    <tr className='striped--light-gray'>
-      <th className='f6 ttu fw6 pv2 ph3'>Name</th>
-      <th className='f6 ttu fw6 pv2 ph3'>Address</th>
-      <th className='f6 ttu fw6 pv2 ph3'>Email</th>
-      <th className='f6 ttu fw6 pv2 ph3'>Phone</th>
-    </tr>
+  <table className='collapse w-100 ba b--black-10 pv2 ph3 center'>
+    <tbody>
+      <tr className='striped--light-gray'>
+        <th className='f6 ttu fw6 pv2 ph3'>Name</th>
+        <th className='f6 ttu fw6 pv2 ph3'>Address</th>
+        <th className='f6 ttu fw6 pv2 ph3'>Email</th>
+        <th className='f6 ttu fw6 pv2 ph3'>Phone</th>
+      </tr>
 
-    { users.map(User) }
-  </tbody>
+      { users.map(User) }
+    </tbody>
+  </table>
 )
